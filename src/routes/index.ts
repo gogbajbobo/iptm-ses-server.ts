c5d175fd54614b7704da5b0b9cd19318b34ae429
gogbajbobo <grigoriev.maxim@gmail.com>
2020-12-23 17:23:06 +0300

    Create routes index

---
+++ src/routes/index.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express'
+import { examGetAllAction, examGetByIdAction, postSaveAction } from '../controller/exam'
+
+export interface Route {
+    path: string;
+    method: 'get' | 'post' | 'put' | 'delete';
+    action:  (req: Request, res: Response) => Promise<void>;
+}
+
+export const AppRoutes: Route[] = [
+    {
+        path: '/exams',
+        method: 'get',
+        // action: postGetAllAction
+        action: examGetAllAction
+    },
+    {
+        path: '/exams/:id',
+        method: 'get',
+        action: examGetByIdAction
+    },
+    {
+        path: '/exams',
+        method: 'post',
+        action: postSaveAction
+    }
+]
