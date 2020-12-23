762c240422ea5483d5a694abba168ece9550d63a
gogbajbobo <grigoriev.maxim@gmail.com>
2020-12-23 12:43:51 +0300

    Initial commit

---
+++ app.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const app = express()
+const PORT = 8000
+app.get(
+    '/',
+    (req, res) => {
+        return res.send('Express + TypeScript Server new')
+    }
+)
+app.get(
+    '/v',
+    (req, res) => {
+        return res.send('1.0.0')
+    }
+)
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
+})
