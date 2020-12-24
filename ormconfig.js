820aa8bfd4cf335a29bb91633c9ba3dc5f0c66d1
gogbajbobo <grigoriev.maxim@gmail.com>
2020-12-24 11:54:55 +0300

    check NODE_ENV to define basePath

--- ormconfig.js
+++ ormconfig.js
@@ -1,19 +1,29 @@
-const basePath = process.env.NODE_ENV === 'development' ? 'src' : 'build'
+const nodeEnv = process.env.NODE_ENV
+const basePath =
+    nodeEnv === 'production'
+        ? 'build'
+        : nodeEnv === 'development'
+            ? 'src'
+            : null
+
+if (!basePath) console.warn(`You should set either 'production' or 'development' NODE_ENV value`)
+
+console.log(`basePath: ${ basePath }`)
 
 module.exports = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  database: 'iptm_ses',
-  synchronize: true,
-  dropSchema: false,
-  entities: [ `${ basePath }/entity/*.{js,ts}` ],
-  subscribers: [ `${ basePath }/subscriber/*.{js,ts}` ],
-  migrations: [ `${ basePath }/migration/*.{js,ts}` ],
-  cli: {
-    entitiesDir: 'src/entity',
-    migrationsDir: 'src/migration',
-    subscribersDir: 'src/subscriber'
-  }
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    database: 'iptm_ses',
+    synchronize: true,
+    dropSchema: false,
+    entities: [ `${ basePath }/entity/*.{js,ts}` ],
+    subscribers: [ `${ basePath }/subscriber/*.{js,ts}` ],
+    migrations: [ `${ basePath }/migration/*.{js,ts}` ],
+    cli: {
+        entitiesDir: 'src/entity',
+        migrationsDir: 'src/migration',
+        subscribersDir: 'src/subscriber'
+    }
 }
