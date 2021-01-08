ba3077d150c6d45446db4b8e264f43f2ac22b89c
gogbajbobo <grigoriev.maxim@gmail.com>
2021-01-08 12:06:07 +0300

    addCategory route

--- src/controller/categories.ts
+++ src/controller/categories.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 import { getRepository, FindManyOptions } from 'typeorm'
 import { Category } from '../entity/Category'
-import { defaultFindOptions } from './_helper'
+import { defaultFindOptions, serverError, rejectedClientError } from './_helper'
 
 export const getCategories = (req: Request, res: Response): Promise<Response> => {
 
@@ -9,6 +9,19 @@ export const getCategories = (req: Request, res: Response): Promise<Response> =>
 
     return getRepository(Category).find(options)
         .then(categories => res.json(categories))
-        .catch(err => res.status(500).json({ error: err.message }))
+        .catch(serverError(res))
+
+}
+
+export const addCategory = (req: Request, res: Response): Promise<Response> => {
+
+    const { category } = req.body
+
+    if (!category)
+        return rejectedClientError(res, 'have no category in request')
+
+    return getRepository(Category).save(category)
+        .then(category => res.json(category))
+        .catch(serverError(res))
 
 }
--- src/routes/api/categories.ts
+++ src/routes/api/categories.ts
@@ -1,6 +1,6 @@
 import { Route } from '../interfaces'
 import passport from '../../services/passport'
-import { getCategories } from '../../controller/categories'
+import { getCategories, addCategory } from '../../controller/categories'
 import { requireRoles } from '../../services/rolesChecker'
 import { UserRole } from '../../entity/UserRole'
 
@@ -15,6 +15,15 @@ const routes: Route[] = [
             requireRoles([UserRole.EXAMINER]),
         ],
         actions: [ getCategories ]
+    },
+    {
+        path,
+        method: 'post',
+        authorize: [
+            passport.authenticate('jwt'),
+            requireRoles([UserRole.EXAMINER]),
+        ],
+        actions: [ addCategory ]
     },
 ]
 
