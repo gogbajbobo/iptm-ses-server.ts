b136263fbf1be0aea31a89cdeaea2b14444abb04
gogbajbobo <grigoriev.maxim@gmail.com>
2021-01-08 14:35:20 +0300

    fix Route interface

--- src/routes/interfaces.ts
+++ src/routes/interfaces.ts
@@ -4,7 +4,7 @@ export type RouteActionResponse = Response | NextFunction | Promise<Response> | 
 export type RouteAction = (req: Request, res: Response, next?: NextFunction) => RouteActionResponse
 
 export interface Route {
-    path: string
+    path: string | string[]
     method: 'get' | 'post' | 'put' | 'delete' | 'all'
     authorize?: RouteAction[]
     actions: RouteAction[]
