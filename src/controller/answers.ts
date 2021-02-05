178e580ac4e41c334331099505f53240cff2e4b8
gogbajbobo <grigoriev.maxim@gmail.com>
2021-02-05 17:01:46 +0300

    updateAnswer

--- src/controller/answers.ts
+++ src/controller/answers.ts
@@ -1,6 +1,60 @@
 import { Answer } from '../entity/Answer'
 import { controller } from './index'
+import { Request, Response } from 'express'
+import { defaultFindOptions, rejectedClientError, serverError } from './_helper'
+import { FindManyOptions, getRepository } from 'typeorm'
 
 const answerController = controller(Answer)
+
+const updateAnswer = (req: Request, res: Response): Promise<Response> => {
+
+    const { id } = req.params
+    const { item } = req.body
+
+    if (!id || !item)
+        return rejectedClientError(res, 'have no id or item')
+
+    const itemRepository = getRepository(Answer)
+
+    return itemRepository.findOne(id)
+        .then(answer => {
+
+            // only text to change or isCorrect set to false
+            if (answer.isCorrect === item.isCorrect || item.isCorrect === false) {
+
+                const { text, isCorrect } = item
+
+                answer.text = text
+                answer.isCorrect = isCorrect
+
+                return itemRepository.save(answer)
+                    .then(result => res.json(result))
+
+            }
+
+            //only isCorrect set to true
+
+            const { questionId } = answer
+
+            const options: FindManyOptions = defaultFindOptions(req)
+            options.where = { questionId, isCorrect: true }
+
+            return itemRepository.find(options)
+                .then(answers => {
+
+                    answers.forEach(a => a.isCorrect = false)
+                    answer.isCorrect = true
+
+                    return itemRepository.save([ ...answers, answer ])
+                        .then(result => res.json(result))
+
+                })
+
+        })
+        .catch(serverError(res))
+
+}
+
+answerController.updateItem = updateAnswer
 
 export default answerController
