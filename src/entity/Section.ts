5dc25f647d6f4b9adfdfe4cb0091252dd64e9174
gogbajbobo <grigoriev.maxim@gmail.com>
2021-02-10 14:54:16 +0300

    use @RelationId decorator instead of @Column

--- src/entity/Section.ts
+++ src/entity/Section.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, OneToMany, ManyToOne } from 'typeorm'
+import {Entity, Column, OneToMany, ManyToOne, RelationId} from 'typeorm'
 import { Exam } from './Exam'
 import { Question } from './Question'
 import { Category } from './Category'
@@ -10,13 +10,13 @@ export class Section extends Datum {
     @Column()
     title: string
 
-    @Column()
-    examId: number
-
     @ManyToOne(() => Exam, exam => exam.sections, {
         onDelete: 'CASCADE',
     })
     exam: Exam
+
+    @RelationId((section: Section) => section.exam)
+    examId: number
 
     @OneToMany(() => Question, question => question.section, {
         eager: true,
