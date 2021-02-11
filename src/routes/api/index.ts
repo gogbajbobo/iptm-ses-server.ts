d1b2ab71e5e1f97b0f09801f4d18d7dec8ccc9a1
gogbajbobo <grigoriev.maxim@gmail.com>
2021-02-11 18:48:00 +0300

    set roles for routes

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -4,13 +4,22 @@ import quizController from '../../controller/quizzes'
 import entity from '../../entity'
 import routes from './_helper'
 import examinees from './examinees'
+import { UserRole } from '../../entity/UserRole'
 
 const categories = routes(`/categories`, controller(entity.Category))
 const exams = routes(`/exams`, controller(entity.Exam))
 const questions = routes(`/questions`, controller(entity.Question))
 const sections = routes(`/sections`, controller(entity.Section))
 const answers = routes(`/answers`, answerController)
-const quizzes = routes(`/quizzes`, quizController)
+
+const quizRoles = {
+    all: [ UserRole.EXAMINER, UserRole.EXAMINEE ],
+    get: [ UserRole.EXAMINER, UserRole.EXAMINEE ],
+    post: [ UserRole.EXAMINER ],
+    put: [ UserRole.EXAMINER ],
+    delete: [ UserRole.EXAMINER ],
+}
+const quizzes = routes(`/quizzes`, quizController, quizRoles)
 
 export default [
 
