306e4786b4b7b92bed6f02cf672a99c035a7b802
gogbajbobo <grigoriev.maxim@gmail.com>
2021-02-11 19:07:51 +0300

    getItems for quizzes
    intermediate commit / not working yet

--- src/controller/quizzes.ts
+++ src/controller/quizzes.ts
@@ -1,10 +1,46 @@
 import { Quiz } from '../entity/Quiz'
+import { User } from '../entity/User'
+import { UserRole } from '../entity/UserRole'
 import { controller } from './index'
 import { Request, Response } from 'express'
-import { rejectedClientError, serverError } from './_helper'
-import { getRepository } from 'typeorm'
+import { defaultFindOptions, rejectedClientError, serverError } from './_helper'
+import {FindManyOptions, getRepository, Raw} from 'typeorm'
 
 const quizController = controller(Quiz)
+
+const getItems = (req: Request, res: Response): Promise<Response> => {
+
+    const { query } = req
+    const user: User = req.user as User
+
+    if (user.roles.includes(UserRole.EXAMINER)) {
+
+        const options: FindManyOptions = defaultFindOptions(req)
+        options.where = query
+
+        return getRepository(Quiz).find(options)
+            .then(items => res.json(items))
+            .catch(serverError(res))
+
+    }
+
+    if (user.roles.includes(UserRole.EXAMINEE)) {
+
+        const options: FindManyOptions = defaultFindOptions(req)
+
+        options.where = {
+            examinees: Raw(alias => `FIND_IN_SET('${ user.id }',${ alias })>0`)
+        }
+
+        return getRepository(Quiz).find(options)
+            .then(items => res.json(items))
+            .catch(serverError(res))
+
+    }
+
+    return rejectedClientError(res, 'unknown user role')
+
+}
 
 const addItem = (req: Request, res: Response): Promise<Response> => {
 
@@ -26,6 +62,7 @@ const addItem = (req: Request, res: Response): Promise<Response> => {
 
 }
 
+quizController.getItems = getItems
 quizController.addItem = addItem
 
 export default quizController
