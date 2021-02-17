88dc9fcc6c1ec2409125ad628b0f63613b142a26
gogbajbobo <grigoriev.maxim@gmail.com>
2021-02-17 16:13:17 +0300

    separate getQuestionsFor(Examinee/Examiner)

--- src/controller/questions.ts
+++ src/controller/questions.ts
@@ -1,8 +1,10 @@
 import { controller } from './index'
 import { Question } from '../entity/Question'
+import { User } from '../entity/User'
 import { Request, Response } from 'express'
 import { FindManyOptions, getRepository } from 'typeorm'
-import { defaultFindOptions, serverError } from './_helper'
+import { defaultFindOptions, rejectedClientError, serverError } from './_helper'
+import { isExaminee, isExaminer } from '../services/helper'
 
 const questionController = controller(Question)
 
@@ -10,18 +12,36 @@ const getItems = (req: Request, res: Response): Promise<Response> => {
 
     const { query } = req
 
-    console.log(query)
+    if (!query)
+        return rejectedClientError(res, 'request w/o query is not allowed')
 
-    if (query?.quiz) {
+    const user: User = req.user as User
+    const { quiz } = query
 
-        const options: FindManyOptions = defaultFindOptions(req)
-        options.where = query
+    if (quiz && isExaminee(user))
+        return getQuestionsForExaminee(req, res, Number(quiz))
 
-        return getRepository(Question).find(options)
-            .then(items => res.json(items))
-            .catch(serverError(res))
+    if (isExaminer(user))
+        return getQuestionsForExaminer(req, res)
 
-    }
+    return rejectedClientError(res, 'unknown user role')
+
+}
+
+const getQuestionsForExaminee = (req: Request, res: Response, quizId: number): Promise<Response> => {
+
+    const options: FindManyOptions = defaultFindOptions(req)
+    // options.where = query
+
+    return getRepository(Question).find(options)
+        .then(items => res.json(items))
+        .catch(serverError(res))
+
+}
+
+const getQuestionsForExaminer = (req: Request, res: Response): Promise<Response> => {
+
+    const { query } = req
 
     const options: FindManyOptions = defaultFindOptions(req)
     options.where = query
