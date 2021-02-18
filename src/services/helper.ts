94264aaaf450c5536dc21eaaf479aba96776c95f
gogbajbobo <grigoriev.maxim@gmail.com>
2021-02-18 13:19:46 +0300

    intDivision

--- src/services/helper.ts
+++ src/services/helper.ts
@@ -8,3 +8,12 @@ export const isExaminer = (user: User): boolean => user.roles.includes(UserRole.
 export const isAdmin = (user: User): boolean => user.roles.includes(UserRole.ADMIN)
 
 export const arrayIntersect = (a1: unknown[], a2: unknown[]): unknown[] => a1.filter(v => a2.includes(v))
+
+export const intDivision = (i1: number, i2: number): Record<'quotient' | 'reminder', number> => {
+
+    const quotient = Math.trunc(i1 / i2)
+    const reminder = i1 % i2
+
+    return { quotient, reminder }
+
+}
