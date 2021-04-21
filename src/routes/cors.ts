c764feeb378e6f27f6e58934ea4103a58161e93b
gogbajbobo <grigoriev.maxim@gmail.com>
2021-04-21 11:37:21 +0300

    temporary disable CORS check

--- src/routes/cors.ts
+++ src/routes/cors.ts
@@ -7,22 +7,22 @@ log.info(`Allowed client origins: ${ allowedOrigins }`)
 
 const corsInit = (req: Request, res: Response, next: NextFunction): RouteActionResponse => {
 
-    log.info(`req.headers: ${ JSON.stringify(req.headers, null, '\t')}`)
+    // if (req.path === '/') {
+    //
+    //     res.header('Access-Control-Allow-Origin', '*')
+    //
+    // } else {
+    //
+    //     const reqOrigin = req.headers['origin']
+    //
+    //     if (!allowedOrigins.includes(reqOrigin))
+    //         return res.status(403).send(`allowedOrigins do not includes ${ reqOrigin }`)
+    //
+    //     res.header('Access-Control-Allow-Origin', reqOrigin)
+    //
+    // }
 
-    if (req.path === '/') {
-
-        res.header('Access-Control-Allow-Origin', '*')
-
-    } else {
-
-        const reqOrigin = req.headers['origin']
-
-        if (!allowedOrigins.includes(reqOrigin))
-            return res.status(403).send(`allowedOrigins do not includes ${ reqOrigin }`)
-
-        res.header('Access-Control-Allow-Origin', reqOrigin)
-
-    }
+    res.header('Access-Control-Allow-Origin', '*')
 
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Headers', 'Content-type, Authorization')
